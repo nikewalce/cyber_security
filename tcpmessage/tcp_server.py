b8bc857bc4026cc99be3f17a95081c048f41c26f
nikewalce <nikewalce1@gmail.com>
2026-02-27 11:26:40 +0300

    Добавил работу с несколькими пользователями

--- tcpmessage/tcp_server.py
+++ tcpmessage/tcp_server.py
@@ -1,22 +1,34 @@
 #для работы с низкоуровневыми протоколами
 import socket
+#Несколько потоков
+import threading
+
+def handle_client(client, addr):
+    print("Connected:", addr)
+    while True:
+        # Прослушиваем и получаем данные по 1 килобайту
+        data = client.recv(1024)
+        if not data:
+            break
+        print(f"Connected:{addr}: {data.decode()}")
+    client.close()
+    print("Disconnected:", addr)
 
 #Создаем место для обмена данными
 #SOCK_STREAM - TCP, SOCK_DGRAM - UDP, AF_INET -IPv4
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Резервируем адрес и порт для прослушивания и приема пакетов информации
 s.bind(('127.0.0.1', 8888))
-#очередь и ограничение на 5 подключений
+#максимум 5 подключений могут ждать в очереди беклога, пока сервер не вызовет accept()
 s.listen(5)
-while 1:
-    try:
-        #addr - данные об отправителе, client - экземпляр класса socket
-        client, addr = s.accept()
-    #обработчик остановки работы программы с клавиатуры
-    except KeyboardInterrupt:
-        s.close()
-        break
-    else:
-        # Прослушиваем и получаем данные по 1 килобайту
-        result =client.recv(1024)
-        print('Message decode:', result.decode('utf-8'))
+print("Server started")
+
+while True:
+    # addr - данные об отправителе, client - экземпляр класса socket
+    client, addr = s.accept()
+    thread = threading.Thread(
+        target=handle_client,
+        args=(client, addr)
+    )
+    thread.start()
+
