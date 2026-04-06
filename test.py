880b3ddd1e5e2f8ab3d677ed7996817be197ea94
nikewalce <nikewalce1@gmail.com>
2026-04-06 19:40:01 +0300

    добавил зависимости

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
+def removeDuplicates(nums) -> int:
+    return len(nums[:2] + [nums[i] for i in range(2, len(nums)) if nums[i] != nums[i-2]]) if len(nums) > 2 else len(nums)
 
-
+print(removeDuplicates([0,0,1,1,1,1,2,3,3])) #7
